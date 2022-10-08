88492ff72ecb6e5d84f1ccbbe26fd189330f3e4d
Alexander <oops.krit@yandex.ru>
2022-10-08 17:24:54 +0300

    fix: frontend and backend

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -5,17 +5,17 @@ const BadRequestError = require('../errors/BadRequestError');
 const NotFoundError = require('../errors/NotFoundError');
 const ConflictError = require('../errors/ConflictError');
 
+const {NODE_ENV, JWT_SECRET} = process.env;
+
 module.exports.login = (req, res, next) => {
-  const { email, password } = req.body;
+  const {email, password} = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign({ _id: user._id }, 'some-secret-key', { expiresIn: '7d' });
+      const token = jwt.sign({_id: user._id}, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', {expiresIn: '7d'},);
       res.cookie('jwt', token, {
-        maxAge: 3600000 * 24 * 7,
-        httpOnly: true,
-        sameSite: true,
+        maxAge: 3600000 * 24 * 7, httpOnly: true, sameSite: true,
       });
-      res.send({ message: 'Авторизация успешна', token });
+      res.send(user.toJSON());
     })
     .catch(next);
 };
@@ -32,7 +32,7 @@ module.exports.getUserById = (req, res, next) => {
       if (!user) {
         throw new NotFoundError('Пользователь не найден');
       }
-      res.send({ data: user });
+      res.send({data: user});
     })
     .catch((error) => {
       if (error.name === 'CastError') {
@@ -44,38 +44,26 @@ module.exports.getUserById = (req, res, next) => {
 };
 
 module.exports.getCurrentUser = (req, res, next) => {
-  const { _id } = req.user;
+  const {_id} = req.user;
   User.findById(_id)
     .then((user) => {
-      res.send({ data: user });
+      res.send({data: user});
     })
     .catch(next);
 };
 
 module.exports.createUser = (req, res, next) => {
   const {
-    name,
-    about,
-    avatar,
-    email,
-    password,
+    name, about, avatar, email, password,
   } = req.body;
 
   bcrypt.hash(password, 10)
     .then((hash) => User.create({
-      name,
-      about,
-      avatar,
-      email,
-      password: hash,
+      name, about, avatar, email, password: hash,
     }))
     .then((user) => {
       res.status(201).send({
-        _id: user._id,
-        name: user.name,
-        about: user.about,
-        avatar: user.avatar,
-        email,
+        _id: user._id, name: user.name, about: user.about, avatar: user.avatar, email,
       });
     })
     .catch((err) => {
@@ -90,13 +78,13 @@ module.exports.createUser = (req, res, next) => {
 };
 
 module.exports.updateUser = (req, res, next) => {
-  const { name, about } = req.body;
-  User.findByIdAndUpdate(req.user._id, { name, about }, { new: true, runValidators: true })
+  const {name, about} = req.body;
+  User.findByIdAndUpdate(req.user._id, {name, about}, {new: true, runValidators: true})
     .orFail(() => {
       throw new BadRequestError('Переданы неверные данные');
     })
     .then((user) => {
-      res.status(200).send({ data: user });
+      res.status(200).send({data: user});
     })
     .catch((err) => {
       if (err.name === 'ValidationError') {
@@ -108,14 +96,14 @@ module.exports.updateUser = (req, res, next) => {
 };
 
 module.exports.updateAvatar = (req, res, next) => {
-  const { avatar } = req.body;
-  User.findByIdAndUpdate(req.user._id, { avatar }, { new: true, runValidators: true })
+  const {avatar} = req.body;
+  User.findByIdAndUpdate(req.user._id, {avatar}, {new: true, runValidators: true})
     .then((user) => {
       if (!user) {
         next(new NotFoundError('Пользователь не найден'));
         return;
       }
-      res.send({ data: user });
+      res.send({data: user});
     })
     .catch((error) => {
       if (error.name === 'ValidationError') {
