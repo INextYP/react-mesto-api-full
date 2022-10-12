823b4f330caf363e32903956792bf8bcefc66ebb
Alexander <oops.krit@yandex.ru>
2022-10-12 13:34:07 +0300

    fix bags

--- backend/app.js
+++ backend/app.js
@@ -10,7 +10,7 @@ const bodyParser = require('body-parser');
 const routerUsers = require('./routes/users');
 const routerCards = require('./routes/cards');
 const { login, createUser } = require('./controllers/users');
-const { auth } = require('./middlewares/auth');
+const { authorization } = require('./middlewares/auth');
 const {
   registrationValidation, loginValidation,
 } = require('./middlewares/validation');
@@ -47,7 +47,7 @@ app.post('/signup', registrationValidation, createUser);
 
 app.post('/signin', loginValidation, login);
 
-app.use(auth);
+app.use(authorization);
 
 app.use('/users', routerUsers);
 
